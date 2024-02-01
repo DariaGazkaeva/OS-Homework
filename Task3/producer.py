ba60ad6e1e15b894db916612f031fa7977492b97
DariaGazkaeva <d.gazkaeva@gmail.com>
2024-02-01 17:26:07 +0000

    added other tasks

---
+++ Task3/producer.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import random
+import time
+
+
+operations = ["+", "-", "*", "/"]
+
+
+def main():
+    n = random.randint(120, 180)
+    for i in range(n):
+        x = random.randint(1, 9)
+        y = random.randint(1, 9)
+        o = operations[random.randint(0, len(operations) - 1)]
+        print(x, o, y, flush=True)
+        time.sleep(1)
+
+
+main()
